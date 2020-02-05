525fcbd30d9d75fb753db6ba00395e1965fa50af
duvanjm <duvanjarin@gmail.com>
2020-02-05 19:35:48 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 main()
 {
- /*This program print a sentence*/
-puts("/""Programming is like building a multilingual puzzle")
+ /*main - print a sentence*/
+  puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
 
