e62d7551c12494c295811e7c32fa6b5904400a98
duvanjm <duvanjarin@gmail.com>
2020-02-07 01:00:43 +0000

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - function
+ *Return: valve 0
+ */
+int main(void)
+{
+  int a;
+  for (a = 0; a < 10; a++)
+    {
+      putchar(a + '0');
+    }
+  putchar ('\n');
+  return(0);
+}
