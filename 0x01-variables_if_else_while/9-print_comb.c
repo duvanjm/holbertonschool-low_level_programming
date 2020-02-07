42fbc0fd556315d9feec7199a19dbab117e29b73
duvanjm <duvanjarin@gmail.com>
2020-02-07 01:21:59 +0000

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - function
+ *Return: valve 0
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+if (i < '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
