7ff8521cdacf292cace6cc2aad7e12a249637f97
duvanjm <duvanjarin@gmail.com>
2020-02-07 00:07:53 +0000

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,10 @@
  */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-char d = 'a';
-while (d <= 'z')
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
-putchar(d);
-d++;
+putchar(c);
 }
 putchar('\n');
 return (0);
