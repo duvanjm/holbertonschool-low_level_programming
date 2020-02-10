541be7c4fe9b27d64c87019aa1b24dc53fce69fc
duvanjm <duvanjarin@gmail.com>
2020-02-10 23:22:00 +0000

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - function
+ *Return: valve 0
+ */
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+  _putchar(x);
+}
+_putchar('\n');
+}
