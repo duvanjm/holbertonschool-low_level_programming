b485e4a0c6411660fcf36a9ca82977191ed1040e
duvanjm <duvanjarin@gmail.com>
2020-02-10 21:21:43 +0000

    0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,19 @@
 #include "holberton.h"
+/**
+ *main - function
+ *Return: valve 0
+ */
 int main(void)
 {
-  putchar('H');
-  putchar('o');
-  putchar('l');
-  putchar('b');
-  putchar('e');
-  putchar('r');
-  putchar('t');
-  putchar('o');
-  putchar('n');
-  putchar('\n'); 
-  return (0);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
 }
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,4 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+int _putchar(char c); 
+#endif
