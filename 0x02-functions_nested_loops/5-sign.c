3be5ed342d2a217a0dd6704e13790edd28508050
duvanjm <duvanjarin@gmail.com>
2020-02-11 17:41:10 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_sign - @coment
+ *@n: function
+ *Return: valve 0
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchari(43);
+return (1);
+}
+else if (n < 0)
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(0);
+return (0);
+}
