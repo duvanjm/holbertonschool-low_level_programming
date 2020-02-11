ac945d834ae536cfe00ac4da306b017ebeac53ae
duvanjm <duvanjarin@gmail.com>
2020-02-11 18:45:43 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,18 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchari(43);
+_putchar(43);
 return (1);
 }
 else if (n < 0)
+{
 _putchar(45);
 return (-1);
 }
 else
 {
-_putchar(0);
+_putchar(48);
+return (0);
+}
 return (0);
 }
