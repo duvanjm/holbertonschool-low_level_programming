66fd700115cc3da77d5644224903cfba0909754b
duvanjm <duvanjarin@gmail.com>
2020-02-11 02:43:49 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdioh>
+/**
+ *main - function
+ *Return: valve 0
+ */
+int _islower(int c)
+{
+char x;
+if(islower %c (x));
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
