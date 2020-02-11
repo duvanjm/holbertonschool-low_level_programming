657af86dbb8eed7c5bf415e69bd0c297a728e97b
duvanjm <duvanjarin@gmail.com>
2020-02-11 21:56:30 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *print_last_digit - print the last digit
+ *@n: function
+ *Return: valve 0
+ */
+int print_last_digit(int n)
+{
+int j;
+j = (n % 10);
+if (j < 0)
+{
+j = j * (-1);
+}
+_putchar(j + 48);
+return (j);
+}
