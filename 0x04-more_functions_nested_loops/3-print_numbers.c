2cd2f84493fe677c24599df198290a5af01a2482
duvanjm <duvanjarin@gmail.com>
2020-02-13 17:33:55 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_numbers - show numbers
+ *Return: valve 0
+ */
+void print_numbers(void)
+{
+int c;
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
