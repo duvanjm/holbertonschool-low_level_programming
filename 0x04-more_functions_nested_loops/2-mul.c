5305210d7a6c03e7a32355964b682274ac5d5b1f
duvanjm <duvanjarin@gmail.com>
2020-02-13 15:50:39 +0000

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
  *mul - multiplicate
- *@a - int
- *@b - int
+ *@a: int
+ *@b: int
  *Return: valve 0
  */
 int mul(int a, int b)
 {
-return (a*b);
+return (a * b);
 }
