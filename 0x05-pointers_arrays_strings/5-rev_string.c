00c9579371aaf00b731bd11f8b068ace04e70d1b
duvanjm <duvanjarin@gmail.com>
2020-02-19 00:03:19 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *rev_string - function
+ *Return: valve 0
+ *@s: variable
+ */
+void rev_string(char *s)
+{
+int a;
+int d;
+int i;
+char aux[1024];
+int m = 0;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	a--;
+	for (d = a; d > -1; d--)
+	{
+		aux[m] = s[d];
+		m++;
+	}
+	for (i = 0; i < a; i++)
+	{
+		s[i] = aux[i];
+	}
+}
