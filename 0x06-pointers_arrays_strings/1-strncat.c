e0cdf93244222f85ab83e7f19455c3a433f4dfdd
duvanjm <duvanjarin@gmail.com>
2020-02-20 20:12:02 +0000

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,22 @@
 #include "holberton.h"
 /**
- **_strcat - function
+ **_strncat - function
  *@src: variable
  *@dest: variable
+ *@n: variable
  *Return: valve 0
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i;
-  int a;
-  
-  for (i = 0; dest[i] != '\0'; i++)
-    ;
-  for (a = 0; a < n && src[a] != '\0'; a++)
-    {
-    dest[a + i] = src[a];
-    }
-    
-  return (dest);  
+int i;
+int a;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+	dest[a + i] = src[a];
+	}
+	return (dest);
 }
