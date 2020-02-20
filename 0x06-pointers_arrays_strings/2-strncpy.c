a8692ef2aa47e6df67fe9f38e25efc7d9ac4bfff
duvanjm <duvanjarin@gmail.com>
2020-02-20 22:36:51 +0000

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ **_strcat - function
+ *@src: variable
+ *@dest: variable
+ *@n: variable
+ *Return: valve 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+int a;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
