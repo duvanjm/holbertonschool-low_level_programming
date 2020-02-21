8b306b8cb955d705e8c956b347fa2e1c5e1c8c24
duvanjm <duvanjarin@gmail.com>
2020-02-21 02:52:55 +0000

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int count = 0;
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 
-	for (count = 0; s1[count] != '\0'; count++)
-	{
-	if (s1[count] > s2[count])
-	{
-	return (s1[count] - s2[count]);
-	}
-	else if (s1[count] < s2[count])
-	return (s1[count] - s2[count]);
-	(count++);
-	}
-	count = 0;
-	return (count);
 }
