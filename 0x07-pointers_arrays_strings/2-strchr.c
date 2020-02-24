9436fa74b4bcd265e61c65c0cdd76a45351ada27
duvanjm <duvanjarin@gmail.com>
2020-02-24 20:59:05 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_strchr - function
+ *@s: variable
+ *@c: variable
+ *Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+	if (*s == c)
+	{
+	return (s);
+	}
+		s++;
+	}
+	return ('\0');
+}
