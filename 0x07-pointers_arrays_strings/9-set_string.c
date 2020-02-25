3e0cdfeffa91558d8d2856d7aa3de1d271679dea
duvanjm <duvanjarin@gmail.com>
2020-02-25 04:02:51 +0000

    9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *set_string - function
+ *@s: variable
+ *@to: variable
+ *Return: NULL
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -8,4 +8,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
+void set_string(char **s, char *to);
 #endif
