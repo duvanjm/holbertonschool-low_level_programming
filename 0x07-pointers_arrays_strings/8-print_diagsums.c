db48e878ae3afa3d0d0e9814d123a4ca3bf06934
duvanjm <duvanjarin@gmail.com>
2020-02-25 20:55:09 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *set_string - function
- *@s: variable
- *@to: variable
+ *print_diagsums - function
+ *@a: variable
+ *@size: variable
  *Return: NULL
  */
 void print_diagsums(int *a, int size)
 {
-  int x = size*size, s1 = 0, s2 = 0, i;
+int x = size * size, s1 = 0, s2 = 0, i;
 
-  for(i = 0; i < x; i = size+1)
-    {
-      s1 +=*a;
-    
-  for(i = x; i >= 0; i = size-1)
-    {
-      s2+=*a;
-    }
-    }
-  printf("%d, %d", s1, s2);
+	for (i = 0; i < x; i = size + 1)
+	{
+		s1 + = *a;
+	}
+	for (i = x; i >= 0; i = size - 1)
+	{
+		s2 + = *a;
+	}
+	printf("%d, %d", s1, s2);
 }
