f85013ae574ef68de733b08f0a930be764140f85
duvanjm <duvanjarin@gmail.com>
2020-02-25 20:56:32 +0000

    8-print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,11 @@ int x = size * size, s1 = 0, s2 = 0, i;
 
 	for (i = 0; i < x; i = size + 1)
 	{
-		s1 + = *a;
+		s1 += *a;
 	}
 	for (i = x; i >= 0; i = size - 1)
 	{
-		s2 + = *a;
+		s2 += *a;
 	}
 	printf("%d, %d", s1, s2);
 }
