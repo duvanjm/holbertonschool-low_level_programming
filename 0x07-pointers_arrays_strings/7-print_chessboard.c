cd5b57b5966ace85e6715cbee88b8f0bae3061bf
duvanjm <duvanjarin@gmail.com>
2020-02-25 01:13:41 +0000

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *print_chessboard - function
+ *@a: variable
+ *Return: dest
+ */
+void print_chessboard(char (*a)[8])
+{
+int colums;
+int rows;
+
+	for (rows = 0; rows < 8; rows++)
+	{
+		for (colums = 0; colums < 8; colums++)
+		{
+			_putchar(a[rows][colums]);
+		}
+	}
+	_putchar ('\n');
+}
