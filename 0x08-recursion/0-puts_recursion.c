c10d811df639a880720c2dfb77362c306869f00d
duvanjm <duvanjarin@gmail.com>
2020-02-26 17:00:12 +0000

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - function
+ *Return: valve 0
+ *@s: variable
+ */
+void _puts_recursion(char *s)
+{
+int p = 0;
+
+	if (s[p] != '\0')
+	{
+		_putchar (s[p]);
+		p++;
+		_puts_recursion(s + 1);
+	}
+}
