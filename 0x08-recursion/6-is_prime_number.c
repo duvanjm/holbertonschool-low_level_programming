48f044d7b3dbd615fffc65fc1f109dde95e9961e
duvanjm <duvanjarin@gmail.com>
2020-02-27 00:20:48 +0000

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+int _functiond(int y, int x);
+/**
+ *is_prime_number - function
+ *Return: valve 0
+ *@n: variable
+ */
+int is_prime_number(int n)
+{
+
+	if (n <= 0)
+	{
+		return (0);
+	}
+	return (_functiond(n, n - 1));
+}
+/**
+ * _functiond - function
+ * @x: variable
+ * @y: variable
+ * Return: functiond
+ */
+int _functiond(int y, int x)
+{
+	if (x == 2)
+	{
+		return (1);
+	}
+	if (x <= 1)
+	{
+		return (0);
+	}
+	if (y % x == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_functiond(y, x - 1));
+	}
+}
