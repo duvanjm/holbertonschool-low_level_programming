bba9585ad9c89dd393c5742d44cd71243eba7e14
duvanjm <duvanjarin@gmail.com>
2020-03-01 20:34:21 +0000

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main
+ *Return: zero
+ *@argc: variable
+ *@argv: variable
+ */
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc < 3)
+	{
+		printf("Error \n");
+		return (1);
+	}
+	else
+	{
+		int x = atoi(argv[1]);
+		int y = atoi(argv[2]);
+
+		mul = x * y;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
