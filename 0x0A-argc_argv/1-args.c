b72553b1903f2815d48bda07215b4a999f76b3a5
duvanjm <duvanjarin@gmail.com>
2020-03-01 19:29:52 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - main
+ *Return: zero
+ *@argc: variable
+ *@argv: variable
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
