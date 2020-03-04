f8f03d592116b031dc8ad8dce62e029d4b44b581
duvanjm <duvanjarin@gmail.com>
2020-03-04 03:33:07 +0000

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *alloc_grid - function
+ *Return: int
+ *@width: variable
+ *@height: variable
+ */
+int **alloc_grid(int width, int height)
+{
+	int a;
+	char *d;
+
+	if (width == 0)
+	{
+		return (NULL);
+	}
+	d = malloc((width + height)* sizeof(char));
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	for (a = height; a < width; a++)
+	{
+		d[a] = width;
+	}
+	return (0);
+}
