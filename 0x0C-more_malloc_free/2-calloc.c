abed6e67bf89ad58d6f2cd9fbfe1b9239b009dfc
duvanjm <duvanjarin@gmail.com>
2020-03-05 22:58:42 +0000

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *_calloc - function
+ *Return: pointer
+ *@size: variable
+ *@nmemb: variable
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+	{
+		return (NULL);
+	}
+	a = malloc(nmemb * size);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		a[i] = 0;
+	}
+	return (a);
+}
