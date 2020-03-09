c82c43e881eaa40234111129d82b53a1d3f21e04
duvanjm <duvanjarin@gmail.com>
2020-03-09 18:58:02 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+/**
+ * struct dog - Short description
+ * @name:struct
+ * @age: struct
+ * @owner: struct
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
