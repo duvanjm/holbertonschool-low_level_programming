cc3811eb278393f49e9d13be5f615e04cdd000f5
duvanjm <duvanjarin@gmail.com>
2020-03-19 14:08:25 -0500

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,13 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		if (str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->next);
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		i++;
