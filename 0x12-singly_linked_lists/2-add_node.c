2488bf2bc3af68d1dc6287e7a5e46745b9fe513a
duvanjm <duvanjarin@gmail.com>
2020-03-19 17:43:26 -0500

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,10 +6,23 @@
   */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *str
+	list_t *node;
+	int i;
+	char *d;
 
-	if (head == NULL)
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
 	{
 		return (NULL);
 	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	d = strdup(str);
+	node->str = d;
+	node->len = i;
+	node->next = *head;
+	*head = node;
+
+	return (node);
 }
