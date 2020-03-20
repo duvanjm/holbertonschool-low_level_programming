82409655656d830578b3345ea23079c127bb1037
duvanjm <duvanjarin@gmail.com>
2020-03-19 20:09:01 -0500

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,5 +7,70 @@
   */
 list_t *add_node_end(list_t **head, const char *str)
 {
-return (0);
+	list_t *d;
+	list_t *node;
+
+	d = *head;
+	
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	node = newnode(str);
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = node;
+		return (*head);
+	}
+	while (d->next != NULL)
+	{
+		d = d->next;
+	}
+	d->next = node;
+	return (*head);
+}
+
+
+/**
+  *newnode - function
+  *@str: pointer
+  *Return: pointer node
+  */
+
+list_t *newnode(const char *str)
+{
+	list_t *node;
+
+	node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
+	node->str = strdup(str);
+	node->len = lenght(str);
+	node->next = NULL;
+	return (node);
+}
+
+/**
+  *lenght - function
+  *@str: pointer
+  *Return: int
+  */
+
+int lenght(const char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
