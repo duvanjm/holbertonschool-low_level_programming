2837d7a4068130ea00e8d13ca9d555de23871aac
duvanjm <duvanjarin@gmail.com>
2020-03-23 19:28:42 -0500

    9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index - function
+ *@head: pointer, beging of linked list
+ *@idx: index
+ *@n: variable
+ *Return: NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *d;
+	listint_t *node;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+	d = *head;
+	if (d == NULL)
+	{
+		*head = node;
+		node->next = NULL;
+		return (node);
+	}
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	while (i != (idx - 1))
+	{
+		d = d->next;
+		i++;
+		if (d == NULL)
+		{
+			free(node);
+			return (NULL);
+		}
+	}
+	node->next = d->next;
+	d->next = node;
+	return (node);
+}
