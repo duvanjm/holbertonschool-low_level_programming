2409a337df9bb6b89315634234461ff787f2a891
duvanjm <duvanjarin@gmail.com>
2020-03-24 16:56:27 -0500

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *reverse_listint - function
+ *@head: pointer, firts node of list
+ *Return: listint_t
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	int i;
+
+	while (head  != NULL)
+	{
+		i++;
+	}
+
+	return (0);
+}
