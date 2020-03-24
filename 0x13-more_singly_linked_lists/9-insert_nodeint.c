c26f4b130821132d6fc89ef2f99a14530b9532be
duvanjm <duvanjarin@gmail.com>
2020-03-24 09:27:47 -0500

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,8 +10,8 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *d;
-	listint_t *node;
+	listint_t *d = NULL;
+	listint_t *node = NULL;
 
 	if (head == NULL)
 	{
