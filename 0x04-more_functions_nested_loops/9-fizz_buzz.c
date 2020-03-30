eb33bdb7c26c3474f98f9348f4235373b7684dc2
duvanjm <duvanjarin@gmail.com>
2020-03-30 15:13:05 -0500

    9-fizz_buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		if (i % 3 && i % 5)
+		{
+			printf("%d", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
