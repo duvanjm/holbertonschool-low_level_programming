db6e142833e3ec29787f0c4487df97625642e285
duvanjm <duvanjarin@gmail.com>
2020-03-31 14:32:14 -0500

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,8 +26,8 @@ int create_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		w = write(o, text_content, i);
-		if (w == -1)
-			return (0);
+		if (w < 0)
+			return (-1);
 	}
 	close(o);
 	return (1);
