39333c8161ecac39002ae92df3eca5f8426c128e
Duvan <duvanjarin@gmail.com>
2020-05-03 16:03:02 -0500

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - function that returns the nth node of a dlistint_t linked list.
+ * get_dnodeint_at_index - function that returns the nth
+ * node of a dlistint_t linked list.
  * @head: head of the list (firts node)
  * @index: index
  * Return: returns the nth node of a dlistint_t linked list.
