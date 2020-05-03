0b446f10379ef4b26ec896ccb02333abd55be858
duvanjm <duvanjarin@gmail.com>
2020-05-03 01:16:39 -0500

    fix task zero

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,7 +16,7 @@ size_t print_dlistint(const dlistint_t *h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		it++;
+		i++;
 	}
 	return (i);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,8 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t print_dlistint(const dlistint_t *h);
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -21,5 +19,7 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 
 #endif
