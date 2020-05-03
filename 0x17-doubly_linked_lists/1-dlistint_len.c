4f12d2af981987027a135204ebb8717776650c90
Duvan <duvanjarin@gmail.com>
2020-05-02 20:31:08 -0500

    add task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -6,15 +6,15 @@
  * Return: size_t
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i = 0;
+size_t i = 0;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
+
 	return (i);
 }
