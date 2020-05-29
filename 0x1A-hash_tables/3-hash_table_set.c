7504624f9956bbd4c6c17f46ad6ac0454a1673b3
Duvan <duvanjarin@gmail.com>
2020-05-29 11:14:04 -0500

    fix 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	i = key_index((const unsigned char *)key, ht->size);
+	i = key_index((const char *)key, ht->size);
 	new = ht->array[i];
 	while (new != NULL)
 	{
