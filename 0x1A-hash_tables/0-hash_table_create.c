a4b5ef38cfa43df18757ab374697af4dab7026a6
Duvan <duvanjarin@gmail.com>
2020-05-29 10:57:00 -0500

    fix 0-hash_table_create.ch

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *p;
 
-	if (size == NULL)
+	if (!size)
 		return(NULL);
 	p = malloc(sizeof(hash_table_t));
 	if (p == NULL)
