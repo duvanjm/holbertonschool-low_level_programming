059b970983f2244b24da660c74f1e94337805fad
Duvan <duvanjarin@gmail.com>
2020-05-29 11:05:09 -0500

    fix Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *p = NULL;
 
 	if (!size)
-		return(NULL);
+		return (NULL);
 	p = malloc(sizeof(hash_table_t));
 	if (p == NULL)
 		return (NULL);
