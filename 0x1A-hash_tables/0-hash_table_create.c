58f3c893cc3398c3a9accf6005c56910fc60b5e4
Duvan <duvanjarin@gmail.com>
2020-05-29 10:27:48 -0500

    add tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,16 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *p;
 
-	p = malloc(sizeof(hash_node_t));
+	p = malloc(sizeof(hash_table_t));
 	if (p == NULL)
 		return (NULL);
 	p->size = size;
-	p->array = malloc(sizeof(hash_table_t) * size);
+	p->array = calloc(sizeof(hash_node_t), size);
 	if (p->array == NULL)
 	{
-		free(p);
 		return (NULL);
 	}
-	free(p);
 	return (p);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function that adds element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	unsigned long int i;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	i = key_index((const unsigned char *)key, ht->size);
+	new = ht->array[i];
+	while (new != NULL)
+	{
+		if (strcmp(key, new->key))
+		{
+			free(new->value);
+			new->value = strdup(value);
+			return (1);
+		}
+	}
+	new = calloc(sizeof(hash_node_t), 1);
+	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (ht->array[i] == NULL)
+	{
+		ht->array[i] = new;
+		return (1);
+	}
+	new->next = ht->array[i];
+	ht->array = NULL;
+
+	return (1);
+}
