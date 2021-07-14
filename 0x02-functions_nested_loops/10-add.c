75375ca5f08fb62075345870754b32f797e80496
duvanjm <duvanjarin@gmail.com>
2021-07-14 14:45:47 -0500

    add task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - function that sum two integers
+ * @a: integer to add
+ * @b: integer to add
+ * Return: sum of tow integers
+ **/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
