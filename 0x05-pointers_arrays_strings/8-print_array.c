20dc20842e49071796ac73ec824bca5e868be07a
duvanjm <duvanjarin@gmail.com>
2021-07-16 14:37:40 -0500

    add task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - function that prints an array
+ * @a: array
+ * @n: elemets to print in the array
+ * Return: void
+ **/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (i >= n - 1)
+		{
+			printf("%d", a[i]);
+			break;
+		}
+		printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
