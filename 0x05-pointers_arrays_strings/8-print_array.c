4df063110c970440c8992ab2b051216f1ac6fbf5
duvanjm <duvanjarin@gmail.com>
2021-07-16 14:43:41 -0500

    fix task

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,12 +10,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
 		{
 			printf("%d", a[i]);
-			break;
 		}
 		printf("%d, ", a[i]);
 	}
